270c44de0fba1ab70af35012b42da896e1306715
Roberto C. Hernandez <carlosh95@gmail.com>
2015-12-15 19:59:21 -0500

    ajax request working, returning object, not able to render pictures yet tho.

--- js/main.js
+++ js/main.js
@@ -3,18 +3,16 @@
     var api_key = '74e47a159e15cbcb6139ba9c9df64c13';
     var secret = 'c73c5dcbac8e4a96';
     var method = "flickr.photos.search";
-    var user_id = "&user_id=138698049@N03";
+    var user_id = "138698049@N03";
     var format = '&format=json';
-    var url = "https://api.flickr.com/services/rest"+"?method="+method+"&api_key="+api_key+user_id+format+'&nojsoncallback=1';
+    var url = "https://api.flickr.com/services/rest"+"?method="+method+"&api_key="+api_key+"&user_id="+user_id+format+'&nojsoncallback=1';
 
     //required values for oauth
-    var oauth_nonce = makeNonce();
-    var oauth_timestamp = Math.round((new Date()).getTime() / 1000);
-    var oauth_consumer_key = api_key;
-    //var oauth_signature_method= HMAC-SHA1;
-    var oauth_signature = '';
-
-
+    //var oauth_nonce = makeNonce();
+    //var oauth_timestamp = Math.round((new Date()).getTime() / 1000);
+    //var oauth_consumer_key = api_key;
+    //var oauth_signature_method= "HMAC-SHA1";
+    //var oauth_signature = '';
 
 
 
@@ -24,15 +22,19 @@
         xmlhttp.onreadystatechange = function () {
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                 //make an empty array
-                var arr = [];
+                var pics = [];
+
+                //parse request to json
                 var myResp = JSON.parse(xmlhttp.response);
 
-                for(var i=0; i<myResp.photos.photo.length; i++){
-                    console.log(myResp.photos.photo[i].id);
-                }
+                //console.log(myResp);
 
-                document.getElementById("demo").innerHTML = myResp.photos.page;
+                for(var i=0; i< myResp.photos.photo.length; i++){
+                    pics.push(myResp.photos.photo[i]);
+                }
+                console.log(pics);
             }
+            return pics;
         };
 
         xmlhttp.open("Get", url, true);
@@ -41,19 +43,15 @@
     }
     loadPics();
 
-
-
-
-
-
-    function makeNonce()
-    {
-        var text = "";
-        var possible = "0123456789";
-
-        for( var i=0; i < 8; i++ )
-            text += possible.charAt(Math.floor(Math.random() * possible.length));
-
-        return text;
-    }
+    //function to create an oauth_nounce
+    //function makeNonce()
+    //{
+    //    var text = "";
+    //    var possible = "0123456789";
+    //
+    //    for( var i=0; i < 8; i++ )
+    //        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    //
+    //    return text;
+    //}
 })();
