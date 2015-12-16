69aee2fa75e1f20ac98d6746e6fd66676b9be69f
Roberto C. Hernandez <carlosh95@gmail.com>
2015-12-16 16:52:41 -0500

    Making changes to convert to JQuery

--- js/main.js
+++ js/main.js
@@ -1,3 +1,56 @@
 (function(){
+    (function loadPics(){
+        //supplied values from flickr
+        var api_key = '74e47a159e15cbcb6139ba9c9df64c13';
+        var secret = 'c73c5dcbac8e4a96';
+        var method = "flickr.photos.search";
+        var user_id = "138698049@N03";
+        var format = '&format=json';
+        var url = "https://api.flickr.com/services/rest"+"?method="+method+"&api_key="+api_key+"&user_id="+user_id+format+'&nojsoncallback=1';
+
+
+
+        $.getJSON(url, function(data){
+            var items = [];
+            var farm, server, id, title, secret, picUrl, list, img, items;
+
+            if (data) {
+
+                for(var i=0; i< data.photos.photo.length; i++){
+                    farm = data.photos.photo[i].farm;
+                    server = data.photos.photo[i].server;
+                    id = data.photos.photo[i].id;
+                    title = data.photos.photo[i].title;
+                    secret = data.photos.photo[i].secret;
+                    picUrl = "https://farm"+farm+".staticflickr.com/"+server+"/"+id+"_"+secret+"_z.jpg";
+                    $('.image-slider-wrapper ul').append("<li class='active'><img src= "+picUrl+" /></li>");
+                }
+
+                //getCarousel();
+            }
+        });
+
+        $("ul#list").children().addClass('active');
+    })();
 
 })();
+
+
+
+
+
+
+
+
+$( ".myh1" ).html('<bold>Header</bold>');
+
+
+
+
+
+
+
+
+$('.button').on('click', function(){
+    alert("you have clicked");
+})
