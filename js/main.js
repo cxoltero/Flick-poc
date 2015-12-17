0d6be7e94118e9e8962c4a8d1f66c49f9178bdbf
Roberto C. Hernandez <carlosh95@gmail.com>
2015-12-16 20:44:56 -0500

    cleaned code, just need to figure out how to style original pictures

--- js/main.js
+++ js/main.js
@@ -1,26 +1,29 @@
-(function() {
-    "use strict";
+(function(){
+    function loadPics(){
+        //supplied values from flickr
+        var api_key = '74e47a159e15cbcb6139ba9c9df64c13';
+        var secret = 'c73c5dcbac8e4a96';
+        var method = "flickr.photos.search";
+        var user_id = "138698049@N03";
+        var format = '&format=json';
+        var url = "https://api.flickr.com/services/rest"+"?method="+method+"&api_key="+api_key+"&user_id="+user_id+format+'&nojsoncallback=1';
 
-    function loadPics() {
-        var api_key, method, user_id, format, url, xmlhttp;
 
-        api_key = '74e47a159e15cbcb6139ba9c9df64c13';
-        method = "flickr.photos.search";
-        user_id = "138698049@N03";
-        format = '&format=json';
-        url = "https://api.flickr.com/services/rest?method="+method+"&api_key="+api_key+"&user_id="+user_id+format+'&nojsoncallback=1';
-        xmlhttp = new XMLHttpRequest();
 
-        xmlhttp.onreadystatechange = function () {
-            var items;
-            if (xmlhttp.readyState === 4 && xmlhttp.status === 200) {
-                var pics = [];
-                var myResp = JSON.parse(xmlhttp.response);
-                photoData(pics, myResp);
-                pupulate(pics);
-                items = document.getElementById('img0');
-                removeActiveAddCurrent(items);
-                getCarousel();
+        $.getJSON(url, function(data){
+            var items = [];
+            var farm, server, id, title, secret, picUrl, list, img, items;
+
+            if (data) {
+                for(var i=0; i< data.photos.photo.length; i++){
+                    farm = data.photos.photo[i].farm;
+                    server = data.photos.photo[i].server;
+                    id = data.photos.photo[i].id;
+                    title = data.photos.photo[i].title;
+                    secret = data.photos.photo[i].secret;
+                    picUrl = "https://farm"+farm+".staticflickr.com/"+server+"/"+id+"_"+secret+"_z.jpg";
+                    $('.image-slider-wrapper ul').append("<li class='active' ><a class='lightbox' data-lightbox=image-set"+" href='" + picUrl + "'><img class='active' "+ " src='" +picUrl+"'/></a></li>");
+                }
             }
         };
         xmlhttp.open("Get", url, true);
