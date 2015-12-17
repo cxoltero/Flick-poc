9932e971bba09c6ad2d265c53f07ce2b03a5eb03
Roberto C. Hernandez <carlosh95@gmail.com>
2015-12-17 13:26:42 -0500

    removed bower_components and .idea file

--- js/main.js
+++ js/main.js
@@ -1,11 +1,5 @@
-<<<<<<< 91be62623f0cc3695ee87964be1fa4fe7b60df0e
 (function(){
     function loadPics(){
-=======
-
-(function(){
-    (function loadPics(){
->>>>>>> fixing conflics
         //supplied values from flickr
         var api_key = '74e47a159e15cbcb6139ba9c9df64c13';
         var secret = 'c73c5dcbac8e4a96';
@@ -16,10 +10,6 @@
 
 
 
-<<<<<<< 91be62623f0cc3695ee87964be1fa4fe7b60df0e
-=======
-
->>>>>>> fixing conflics
         $.getJSON(url, function(data){
             var items = [];
             var farm, server, id, title, secret, picUrl, list, img, items;
@@ -34,90 +24,8 @@
                     picUrl = "https://farm"+farm+".staticflickr.com/"+server+"/"+id+"_"+secret+"_z.jpg";
                     $('.image-slider-wrapper ul').append("<li class='active' ><a class='lightbox' data-lightbox=image-set"+" href='" + picUrl + "'><img class='active' "+ " src='" +picUrl+"'/></a></li>");
                 }
-<<<<<<< 91be62623f0cc3695ee87964be1fa4fe7b60df0e
             }
-        }
-        xmlhttp.open("Get", url, true);
-        xmlhttp.send();
-    }
-    function getCarousel(){
-        var next, counter, prev, items, amount, current;
-
-        next = document.getElementById('next');
-        counter = 0;
-        prev = document.getElementById('prev');
-        items = document.getElementsByTagName('img');
-        amount = items.length;
-        current = document.getElementsByClassName('current');
-
-        function navigate(direction){
-            if(direction === 1){
-                items = document.getElementById('img'+counter);
-                current = document.getElementsByClassName('current');
-                removeCurrentAddActive(items);
-            }
-        }
-        next.addEventListener("click", function(){
-            items = document.getElementById('img'+counter);
-            counter = counter+1;
-
-            if(counter <= amount-1){
-                removeCurrentAddActive(items);
-                removeActiveAddCurrent(items.nextSibling);
-            }else if(counter === amount){
-                removeCurrentAddActive(items);
-                items = document.getElementById('img0');
-                removeActiveAddCurrent(items);
-                counter=0;
-            }
-            navigate(0);
         });
-        prev.addEventListener("click", function(){
-            items = document.getElementById('img'+counter);
-
-            if(counter > 0){
-                counter = counter-1;
-                removeCurrentAddActive(items);
-                removeActiveAddCurrent(items.previousSibling);
-            }else if(counter === 0){
-                items = document.getElementById('img'+counter);
-                removeCurrentAddActive(items);
-                counter = amount-1;
-                items = document.getElementById('img'+counter);
-                removeActiveAddCurrent(items);
-            }
-            navigate(-1);
-        });
-    }
-    function photoData(pics, myResp){
-        /**
-         * @param myResp          Information about the object.
-         * @param myResp.photos   Information about the object's members.
-         * @param myResp.photos.photo   Information about the object's members.
-         * @param myResp.photos.photo.farm   Information about the object's members.
-         * @param myResp.photos.photo.server   Information about the object's members.
-         * @param myResp.photos.photo.secret   Information about the object's members.
-         */
-        var farm, server, id, secret, picUrl;
-
-        for(var i=0; i< myResp.photos.photo.length; i++){
-            farm = myResp.photos.photo[i].farm;
-            server = myResp.photos.photo[i].server;
-            id = myResp.photos.photo[i].id;
-            secret = myResp.photos.photo[i].secret;
-            picUrl = "https://farm"+farm+".staticflickr.com/"+server+"/"+id+"_"+secret+"_z.jpg";
-            pics.push(picUrl);
-        }
-    }
-    function pupulate(pics){
-        for(var j=0; j<pics.length; j++){
-            var list = document.getElementById('list');
-            for(j = 0; j<pics.length; j++){
-                list.innerHTML = list.innerHTML + "<li class= 'active' id='img"+[j]+"'><img src='" + pics[j] + "' /></li>";
-=======
->>>>>>> fixing conflics
-            }
-        }
     }
     function removeActiveAddCurrent(items){
         items.classList.remove('active');
