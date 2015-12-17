b7a5e0b9730d67b9202b2ff40de292e85d91eabb
Roberto C. Hernandez <carlosh95@gmail.com>
2015-12-17 15:43:29 -0500

    refactored functions, and deleted unnecessary items

--- js/main.js
+++ js/main.js
@@ -1,39 +1,38 @@
 (function(){
     (function loadPics(){
-        //supplied values from flickr
-        var api_key = '74e47a159e15cbcb6139ba9c9df64c13';
-        var secret = 'c73c5dcbac8e4a96';
-        var method = "flickr.photos.search";
-        var user_id = "138698049@N03";
-        var format = '&format=json';
-        var url = "https://api.flickr.com/services/rest"+"?method="+method+"&api_key="+api_key+"&user_id="+user_id+format+'&nojsoncallback=1';
+        var api_key, method, user_id, format, url;
+
+        api_key = '74e47a159e15cbcb6139ba9c9df64c13';
+        method = "flickr.photos.search";
+        user_id = "138698049@N03";
+        format = '&format=json';
+        url = "https://api.flickr.com/services/rest"+"?method="+method+"&api_key="+api_key+"&user_id="+user_id+format+'&nojsoncallback=1';
 
         $.getJSON(url, function(data){
-            var items = [];
-            var farm, server, id, title, secret, picUrl, list, img, items;
-
-            console.log(data.photos.photo.length);
+            var farm, server, id, title, secret, picUrl, img;
             if (data) {
-                for(var i=0; i< data.photos.photo.length; i++){
-                    farm = data.photos.photo[i].farm;
-                    server = data.photos.photo[i].server;
-                    id = data.photos.photo[i].id;
-                    title = data.photos.photo[i].title;
-                    secret = data.photos.photo[i].secret;
-                    picUrl = "https://farm"+farm+".staticflickr.com/"+server+"/"+id+"_"+secret+"_z.jpg";
-                    //$('.image-slider-wrapper ul').append("<li class='active' ><a class='lightbox' data-lightbox=image-set"+" href='" + picUrl + "'><img class='active' "+ " src='" +picUrl+"'/></a></li>");
-                    $('.carousel-inner').append("<div class='item' ><img src='" +picUrl+"'/></div>");
-                }
+                createDomElm(data);
                 addActiveclass();
             }
         });
+
+        function createDomElm(data){
+            for(var i=0; i< data.photos.photo.length; i++){
+                farm = data.photos.photo[i].farm;
+                server = data.photos.photo[i].server;
+                id = data.photos.photo[i].id;
+                title = data.photos.photo[i].title;
+                secret = data.photos.photo[i].secret;
+                picUrl = "https://farm"+farm+".staticflickr.com/"+server+"/"+id+"_"+secret+"_z.jpg";
+                $('.carousel-inner').append("<div class='item' ><img src='" +picUrl+"'/></div>");
+            }
+        }
         function addActiveclass(){
             $('.carousel').carousel({
                 interval:3000
             });
             $(".item:first-child").addClass('active');
         };
-
     })();
 
 })();
