105717805d2eca12269ca34ee4d93cb71931fcad
Roberto C. Hernandez <carlosh95@gmail.com>
2015-12-17 11:02:20 -0500

    fixing conflics

--- js/main.js
+++ js/main.js
@@ -1,12 +1,18 @@
+
+
+
 (function(){
     "use strict";
 
     (function loadPics(){
+
+        //supplied values from flickr
         var api_key = '74e47a159e15cbcb6139ba9c9df64c13';
         var method = "flickr.photos.search";
         var user_id = "138698049@N03";
         var format = '&format=json';
         var url = "https://api.flickr.com/services/rest"+"?method="+method+"&api_key="+api_key+"&user_id="+user_id+format+'&nojsoncallback=1';
+
 
         $.getJSON(url, function(data){
             if (data) {
