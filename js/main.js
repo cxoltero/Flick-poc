0c33e268fcbb2a8682a637cfec5e75b0731c02a6
Roberto C. Hernandez <carlosh95@gmail.com>
2015-12-17 15:06:15 -0500

    first refactor

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
 (function() {
     function loadPics() {
-        var api_key = '74e47a159e15cbcb6139ba9c9df64c13';
-        var secret = 'c73c5dcbac8e4a96';
-        var method = "flickr.photos.search";
-        var user_id = "138698049@N03";
-        var format = '&format=json';
-        var url = "https://api.flickr.com/services/rest"+"?method="+method+"&api_key="+api_key+"&user_id="+user_id+format+'&nojsoncallback=1';
+        var api_key, method, user_id, format, url, xmlhttp;
 
-        var xmlhttp = new XMLHttpRequest();
+        api_key = '74e47a159e15cbcb6139ba9c9df64c13';
+        method = "flickr.photos.search";
+        user_id = "138698049@N03";
+        format = '&format=json';
+        url = "https://api.flickr.com/services/rest"+"?method="+method+"&api_key="+api_key+"&user_id="+user_id+format+'&nojsoncallback=1';
+        xmlhttp = new XMLHttpRequest();
 
         xmlhttp.onreadystatechange = function () {
-            var farm, server, id, title, secret, picUrl, list, img, items;
+            var farm, server, id, title, secret, picUrl, items;
 
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                 var pics = [];
@@ -24,22 +24,67 @@
                     secret = myResp.photos.photo[i].secret;
                     picUrl = "https://farm"+farm+".staticflickr.com/"+server+"/"+id+"_"+secret+"_z.jpg";
                     pics.push(picUrl);
-                }
-                populate(pics);
+                };
 
+                pupulate(pics);
                 items = document.getElementById('img0');
-                items.classList.remove('active');
-                items.classList.add('current');
-
+                removeActiveAddCurrent(items);
                 getCarousel();
             }
         };
         xmlhttp.open("Get", url, true);
         xmlhttp.send();
     }
+    function getCarousel(){
+        var box, next, counter, prev, items, amount, current;
 
+        next = document.getElementById('next');
+        counter = 0;
+        prev = document.getElementById('prev');
+        items = document.getElementsByTagName('img');
+        amount = items.length;
+        current = document.getElementsByClassName('current');
 
-    function populate(pics){
+        function navigate(direction){
+            if(direction === 1){
+                items = document.getElementById('img'+counter);
+                current = document.getElementsByClassName('current');
+                removeCurrentAddActive(items);
+            }
+        }
+        next.addEventListener("click", function(event){
+            items = document.getElementById('img'+counter);
+            counter = counter+1;
+
+            if(counter <= amount-1){
+                removeCurrentAddActive(items);
+                removeActiveAddCurrent(items.nextSibling);
+            }else if(counter === amount){
+                removeCurrentAddActive(items);
+                items = document.getElementById('img0');
+                removeActiveAddCurrent(items);
+                counter=0;
+            }
+            navigate(0);
+        });
+        prev.addEventListener("click", function(event){
+            items = document.getElementById('img'+counter);
+
+            if(counter > 0){
+                counter = counter-1;
+                removeCurrentAddActive(items);
+                removeActiveAddCurrent(items.previousSibling);
+            }else if(counter === 0){
+                items = document.getElementById('img'+counter);
+                removeCurrentAddActive(items);
+                counter = amount-1;
+                items = document.getElementById('img'+counter);
+                removeActiveAddCurrent(items);
+            }
+            navigate(-1);
+        });
+    };
+    function pupulate(pics){
         for(var i=0; i<pics.length; i++){
             list = document.getElementById('list');
             img = '';
@@ -48,67 +93,16 @@
             }
         }
     }
-
-    function getCarousel(){
-        var box = document.getElementsByClassName('image-slider-wrapper');
-        var next = document.getElementById('next');
-        var counter = 0;
-        var prev = document.getElementById('prev');
-        var items = document.getElementsByTagName('img');
-        var amount = items.length;
-        var current = document.getElementsByClassName('current');
-
-        function navigate(direction){
-            if(direction === 1){
-                items = document.getElementById('img'+counter);
-                current = document.getElementsByClassName('current');
-                items.classList.remove('current');
-                items.classList.add('active');
-            }
-
-        }
-        next.addEventListener("click", function(event){
-            items = document.getElementById('img'+counter);
-            counter = counter+1;
-
-            if(counter <= amount-1){
-                items.classList.remove('current');
-                items.classList.add('active');
-                items.nextSibling.classList.remove('active');
-                items.nextSibling.classList.add('current');
-            }else if(counter === amount){
-                items.classList.remove('current');
-                items.classList.add('active');
-                items = document.getElementById('img0');
-                items.classList.remove('active');
-                items.classList.add('current');
-                counter=0;
-            }
-            navigate(0);
-        });
-        prev.addEventListener("click", function(event){
-            items = document.getElementById('img'+counter);
-
-            if(counter < 0){
-                counter = counter-1;
-                items.classList.remove('current');
-                items.classList.add('active');
-                items.previousSibling.classList.remove('active');
-                items.previousSibling.classList.add('current');
-            }else if(counter === 0){
-                items = document.getElementById('img'+counter);
-                items.classList.remove('current');
-                items.classList.add('active');
-                counter = amount-1;
-                items = document.getElementById('img'+counter);
-                items.classList.remove('active');
-                items.classList.add('current');
-            }
-            navigate(0);
-        });
+    function removeActiveAddCurrent(items){
+        items.classList.remove('active');
+        items.classList.add('current');
     };
+    function removeCurrentAddActive(items){
+        items.classList.remove('current');
+        items.classList.add('active');
+    }
 
     window.onload = function execute(){
         loadPics();
-    };
+    }
 })();
