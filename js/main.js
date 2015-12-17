709755ea76c17522f05dc0cd9d0dd283414a4e0e
Roberto C. Hernandez <carlosh95@gmail.com>
2015-12-17 15:34:53 -0500

    fixed image positioning, and refactor json request

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 (function(){
-    function loadPics(){
+    (function loadPics(){
         //supplied values from flickr
         var api_key = '74e47a159e15cbcb6139ba9c9df64c13';
         var secret = 'c73c5dcbac8e4a96';
@@ -16,25 +16,19 @@
             var farm, server, id, title, secret, picUrl, list, img, items;
 
             if (data) {
-                for(var i=0; i< data.photos.photo.length; i++){
-                    farm = data.photos.photo[i].farm;
-                    server = data.photos.photo[i].server;
-                    id = data.photos.photo[i].id;
-                    title = data.photos.photo[i].title;
-                    secret = data.photos.photo[i].secret;
-                    picUrl = "https://farm"+farm+".staticflickr.com/"+server+"/"+id+"_"+secret+"_z.jpg";
-                    $('.image-slider-wrapper ul').append("<li class='active' ><a class='lightbox' data-lightbox=image-set"+" href='" + picUrl + "'><img class='active' "+ " src='" +picUrl+"'/></a></li>");
-                }
+                createDomElm(data);
             }
         });
-    }
-    function removeActiveAddCurrent(items){
-        items.classList.remove('active');
-        items.classList.add('current');
-    }
-    function removeCurrentAddActive(items){
-        items.classList.remove('current');
-        items.classList.add('active');
-    }
-    window.onload = loadPics();
+        function createDomElm(data){
+            for(var i=0; i< data.photos.photo.length; i++){
+                farm = data.photos.photo[i].farm;
+                server = data.photos.photo[i].server;
+                id = data.photos.photo[i].id;
+                title = data.photos.photo[i].title;
+                secret = data.photos.photo[i].secret;
+                picUrl = "https://farm"+farm+".staticflickr.com/"+server+"/"+id+"_"+secret+"_z.jpg";
+                $('.image-slider-wrapper ul').append("<li class='active imgArray' ><a data-lightbox=image-set"+" href='" + picUrl + "'><img class='active' "+ " src='" +picUrl+"'/></a></li>");
+            }
+        }
+    })();
 })();
