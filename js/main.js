b04b5cbc50fe6408db6f27319980dcfc6f574c6f
Roberto C. Hernandez <carlosh95@gmail.com>
2015-12-21 11:26:07 -0500

    cleaning unnecesary code

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@ $(document).ready(function(){
     "use strict";
 
     (function loadPics(){
-
         var api_key = '74e47a159e15cbcb6139ba9c9df64c13';
         var method = "flickr.photos.search";
         var user_id = "138698049@N03";
@@ -29,9 +28,9 @@ $(document).ready(function(){
                 title = data.photos.photo[i].title;
                 secret = data.photos.photo[i].secret;
                 picUrl = "https://farm"+farm+".staticflickr.com/"+server+"/"+id+"_"+secret+"_z.jpg";
-                $('.image-slider-wrapper ul').append("<li class='active imgArray' ><a data-lightbox='image-set'"+" href='" + picUrl + "'><img class='active' "+ " src='" +picUrl+"'/></a></li>");
-         }
-    }
+                $('.image-slider-wrapper ul').append("<li class='active imgArray' ><a data-lightbox='image-set'" + " href='" + picUrl + "'><img class='active' "+ " src='" +picUrl+"'/></a></li>");
+            }
+        }
     })();
 <<<<<<< c762791afebbb7eb268dbcdb50c83950617b5b02
 });
