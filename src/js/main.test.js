5b906ef67f1f5e8770f86ce2e60fbcab2f412de3
Roberto C. Hernandez <carlosh95@gmail.com>
2015-12-30 10:09:39 -0500

    re-arranged structure, added npm, and tests.

---
+++ src/js/main.test.js
@@ -0,0 +1,16 @@
+var expect = chai.expect;
+
+describe('Test Suite', function(){
+    var spy;
+    beforeEach(function(){
+        spy = sinon.spy(jQuery, 'getJSON');
+    });
+    afterEach(function(){
+        spy.restore();
+    });
+
+    it('should pass', function(){
+        expect(true).to.equal(true);
+        //expect(true).to.be.true();
+    });
+});
