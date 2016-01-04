d2950fa39c5ea32ad9e49ea40f55008ba1be6ce3
Roberto C. Hernandez <carlosh95@gmail.com>
2016-01-04 15:27:58 -0500

    set app.angular to just angular.

--- src/js/services/getImgService.js
+++ src/js/services/getImgService.js
@@ -1,46 +1,47 @@
 (function (angular) {
     'use strict';
 
-    var app = angular.module('flickrPOC');
-    app.service('images', images);
+    angular
+        .module('flickrPOC')
+        .service('images', images);
 
-    function images($http){
-        var service = {
-            loadPics: loadPics,
-            createImagesArray: createImagesArray
-        }
-        return service;
+        function images($http){
+            var service = {
+                loadPics: loadPics,
+                createImagesArray: createImagesArray
+            };
+            return service;
 
-        function loadPics() {
-            return $http.get(_formUrl());
-            function _formUrl(){
-                var api_key = '74e47a159e15cbcb6139ba9c9df64c13';
-                var method = "flickr.photos.search";
-                var user_id = "138698049@N03";
-                var format = '&format=json';
+            function loadPics() {
+                return $http.get(_formUrl());
+                function _formUrl(){
+                    var api_key = '74e47a159e15cbcb6139ba9c9df64c13';
+                    var method = "flickr.photos.search";
+                    var user_id = "138698049@N03";
+                    var format = '&format=json';
 
-                return "https://api.flickr.com/services/rest" + "?method=" + method + "&api_key=" + api_key + "&user_id=" + user_id + format + '&nojsoncallback=1';
+                    return "https://api.flickr.com/services/rest" + "?method=" + method + "&api_key=" + api_key + "&user_id=" + user_id + format + '&nojsoncallback=1';
+                }
             }
-        }
 
-        function createImagesArray(data){
-            var farm, server, id, title, secret, picUrl, currentPhoto;
-            var images = [];
+            function createImagesArray(data){
+                var farm, server, id, title, secret, picUrl, currentPhoto;
+                var images = [];
 
-            for(var i = 0; i < data.photos.photo.length; i++) {
-                    currentPhoto = data.photos.photo[i];
-                    farm = currentPhoto.farm;
-                    server = currentPhoto.server;
-                    id = currentPhoto.id;
-                    title = currentPhoto.title;
-                    secret = currentPhoto.secret;
-                    picUrl = "https://farm" + farm + ".staticflickr.com/" + server + "/" + id + "_" + secret + "_z.jpg";
-                    images.push(picUrl);
+                for(var i = 0; i < data.photos.photo.length; i++) {
+                        currentPhoto = data.photos.photo[i];
+                        farm = currentPhoto.farm;
+                        server = currentPhoto.server;
+                        id = currentPhoto.id;
+                        title = currentPhoto.title;
+                        secret = currentPhoto.secret;
+                        picUrl = "https://farm" + farm + ".staticflickr.com/" + server + "/" + id + "_" + secret + "_z.jpg";
+                        images.push(picUrl);
                 }
 
-            return images;
-        }
+                return images;
+            }
 
-    }
+        }
 })(angular);
 
