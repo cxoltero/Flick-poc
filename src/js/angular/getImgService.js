b20be42127685d198d78537cef1bc5cc8640e0c7
Roberto C. Hernandez <carlosh95@gmail.com>
2016-01-04 10:31:19 -0500

    added services files

---
+++ src/js/angular/getImgService.js
@@ -0,0 +1,39 @@
+(function (angular) {
+    'use strict';
+
+    var app = angular.module('flickrPock', []);
+    app.service('images', function ($http) {
+
+        this.loadPics = function () {
+            return $http.get(_formUrl());
+
+            function _formUrl() {
+                var api_key = '74e47a159e15cbcb6139ba9c9df64c13';
+                var method = "flickr.photos.search";
+                var user_id = "138698049@N03";
+                var format = '&format=json';
+                var url = "https://api.flickr.com/services/rest" + "?method=" + method + "&api_key=" + api_key + "&user_id=" + user_id + format + '&nojsoncallback=1';
+
+                return url;
+
+            }
+        };
+        this.createImagesArray = function(data){
+            var farm, server, id, title, secret, picUrl, currentPhoto;
+            var images = [];
+
+            for(var i = 0; i < data.photos.photo.length; i++) {
+                currentPhoto = data.photos.photo[i];
+                farm = currentPhoto.farm;
+                server = currentPhoto.server;
+                id = currentPhoto.id;
+                title = currentPhoto.title;
+                secret = currentPhoto.secret;
+                picUrl = "https://farm" + farm + ".staticflickr.com/" + server + "/" + id + "_" + secret + "_z.jpg";
+                images.push(picUrl);
+            }
+            return images;
+        };
+
+    });
+})(angular);
---
+++ src/js/angular/getImgService.test.js
@@ -0,0 +1,141 @@
+describe('getImagesService', function(){
+    'use strict';
+
+    var $http;
+    var ImgService;
+
+    beforeEach(module('flickrPock'));
+
+    beforeEach(inject(function(_$http_, _images_){
+        $http = _$http_;
+        ImgService = _images_;
+    }));
+
+    describe('loadPics', function(){
+
+        describe('stub service call', function(){
+            it('should exist', function(){
+                expect(ImgService.loadPics).to.exist;
+            });
+
+            it('should use $http to retrieve data', function(){
+                var spy = sinon.spy($http, 'get');
+                var method = "flickr.photos.search";
+                var api_key = '74e47a159e15cbcb6139ba9c9df64c13';
+                var user_id = "138698049@N03";
+                var mockUrl = 'https://api.flickr.com/services/rest?method=' + method + '&api_key='+ api_key + '&user_id=' + user_id + '&format=json&nojsoncallback=1';
+
+                ImgService.loadPics();
+
+                expect(spy).to.have.been.calledWith(mockUrl);
+            });
+
+            it('should return a promise', function(){
+               expect(ImgService.loadPics().then).to.exist;
+            });
+
+        });
+    });
+    describe('createImagesArray', function(){
+        var sampleJSON = {contentType: "text/json",
+            status: "success",
+
+            photos: {
+                photo: [
+                    {
+                        farm: 6,
+                        id: "23780914905",
+                        isfamily: 0,
+                        isfriend: 0,
+                        ispublic: 1,
+                        owner: "138698049@N03",
+                        secret: "0eb2a77a55",
+                        server: "5764",
+                        title: ""
+                    },
+                    {
+                        farm: 1,
+                        id: "23698470321",
+                        isfamily: 0,
+                        isfriend: 0,
+                        ispublic: 1,
+                        owner: "138698049@N03",
+                        secret: "1d4dc255d0",
+                        server: "693",
+                        title: ""
+                    }
+                ]
+            }
+
+
+    };
+
+        describe('the data taken by createdDomElm', function(){
+            it('should not be undefined', function(){
+                ImgService.createImagesArray(sampleJSON);
+                expect(sampleJSON).should.not.equal('undefined');
+            });
+            it('should not be null', function(){
+                ImgService.createImagesArray(sampleJSON);
+                expect(typeof(sampleJSON)).should.not.equal('null');
+            });
+            it('should take an object', function(){
+                ImgService.createImagesArray(sampleJSON);
+                expect(typeof(sampleJSON)).to.equal('object');
+            });
+            it('should measure the length of the object inside photos.photo ', function(){
+                expect(sampleJSON.photos.photo).should.exist;
+            });
+            it('length of object should return a number', function(){
+                expect(typeof(sampleJSON.photos.photo.length)).to.equal('number');
+            });
+            it('should get the length of the object', function(){
+                expect(sampleJSON.photos.photo.length).to.equal(2);
+            });
+        });
+        describe('create an array with urls inside of it', function(){
+            it('should return something', function(){
+                expect(typeof(ImgService.createImagesArray(sampleJSON))).to.not.equal('null');
+            });
+            it('should get an array of urls as a return', function(){
+                var picsArray = ImgService.createImagesArray(sampleJSON);
+                for(var i = 0; i < picsArray.length; i++) {
+                    expect(typeof(picsArray[i])).to.equal('string');
+                }
+            });
+        });
+        describe('it should concatenate the contents of the given object', function(){
+            var createImagesArray = function(data){
+                var farm, server, id, title, secret, picUrl, currentPhoto;
+                var images = [];
+
+                for(var i = 0; i < data.photos.photo.length; i++) {
+                    currentPhoto = data.photos.photo[i];
+                    farm = currentPhoto.farm;
+                    server = currentPhoto.server;
+                    id = currentPhoto.id;
+                    title = currentPhoto.title;
+                    secret = currentPhoto.secret;
+                    picUrl = "https://farm" + farm + ".staticflickr.com/" + server + "/" + id + "_" + secret + "_z.jpg";
+                    images.push(picUrl);
+                    expect(images[i]).to.equal("https://farm" + farm + ".staticflickr.com/" + server + "/" + id + "_" + secret + "_z.jpg")
+                }
+            };
+            createImagesArray(sampleJSON);
+        });
+
+    });
+});
+
+
+
+
+/*
+DIRECTIVE
+Accept a list of photos
+Generate URL for each photo
+Create a UL to house list
+Create a LI for each photo
+Each photo has click event
+** When photo clicked, open lightbox (check if lightbox class is applied)**
+ */
