3753c784716aa24c122f289a16c8f5f422379b6a
Roberto C. Hernandez <carlosh95@gmail.com>
2016-01-12 15:15:22 -0500

    updated name of gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var validateCssPipeline = require('pipeline-validate-css')();
 
 
-gulp.task('default', function() {
+gulp.task('css:lint', function() {
     return gulp
         .src(['src/**/*.css'])
         .pipe(validateCssPipeline.validateCSS());
