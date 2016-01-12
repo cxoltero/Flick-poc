dc8eaf259c219c690c377bd4debf4303fbb51154
Roberto C. Hernandez <carlosh95@gmail.com>
2016-01-12 15:28:02 -0500

    js lint and css lint using gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
 var gulp = require('gulp');
 var validatePipeline = require('pipeline-validate-js')();
+var validateCssPipeline = require('pipeline-validate-css')();
 
 gulp.task('js:lint', function() {
   return gulp
     .src('./src/js/**/*.js')
     .pipe(validatePipeline.validateJS());
 });
+
+gulp.task('css:lint', function() {
+  return gulp
+      .src('./src/css/**/*.js')
+      .pipe(validateCssPipeline.validateCSS());
+});
+
+gulp.task('default', ['js:lint', 'css:lint']);
