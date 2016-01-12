cb4737cf265093bec581d8eda7b3bb02a1f2c8cc
Roberto C. Hernandez <carlosh95@gmail.com>
2016-01-12 13:54:19 -0500

    js lint working, fixed all the indentation

--- src/js/services/image.service.js
+++ src/js/services/image.service.js
@@ -1,46 +1,45 @@
 'use strict';
 (function (angular) {
-    angular
-        .module('flickrPOC')
-        .service('images', images);
+  angular
+    .module('flickrPOC')
+    .service('images', images);
 
-        function images($http){
-            return {
-                loadPics: loadPics,
-                createImagesArray: createImagesArray
-            };
+  function images($http) {
+    return {
+      loadPics: loadPics,
+      createImagesArray: createImagesArray
+    };
 
-            function loadPics() {
-                return $http.get(_formUrl());
-                function _formUrl(){
-                    var api_key = '74e47a159e15cbcb6139ba9c9df64c13';
-                    var method = "flickr.photos.search";
-                    var user_id = "138698049@N03";
-                    var format = '&format=json';
+    function loadPics() {
+      return $http.get(_formUrl());
+      function _formUrl() {
+        var apiKey = '74e47a159e15cbcb6139ba9c9df64c13';
+        var method = 'flickr.photos.search';
+        var userId = '138698049@N03';
+        var format = '&format=json';
 
-                    return "https://api.flickr.com/services/rest" + "?method=" + method + "&api_key=" + api_key + "&user_id=" + user_id + format + '&nojsoncallback=1';
-                }
-            }
+        return 'https://api.flickr.com/services/rest' + '?method=' + method + '&api_key=' + apiKey + '&user_id=' + userId + format + '&nojsoncallback=1';
+      }
+    }
 
-            function createImagesArray(data){
-                var farm, server, id, title, secret, picUrl;
-                var images = [];
+    function createImagesArray(data) {
+      var farm, server, id, title, secret, picUrl;
+      var images = [];
 
-                angular.forEach(data.photos.photo, function (photo) {
+      angular.forEach(data.photos.photo, function (photo) {
 
-                    farm = photo.farm;
-                    server = photo.server;
-                    id = photo.id;
-                    title = photo.title;
-                    secret = photo.secret;
-                    picUrl = "https://farm" + farm + ".staticflickr.com/" + server + "/" + id + "_" + secret + "_z.jpg";
+        farm = photo.farm;
+        server = photo.server;
+        id = photo.id;
+        title = photo.title;
+        secret = photo.secret;
+        picUrl = 'https://farm' + farm + '.staticflickr.com/' + server + '/' + id + '_' + secret + '_z.jpg';
 
-                    images.push(picUrl);
-                });
+        images.push(picUrl);
+      });
 
-                return images;
-            }
-
-        }
+      return images;
+    }
+  }
 })(angular);
 
