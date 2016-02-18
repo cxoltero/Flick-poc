d2cb141900216be43dadf474213a76930d51c57b
Roberto C. Hernandez <carlosh95@gmail.com>
2016-02-18 10:03:52 -0500

    app is working as expected

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,9 @@
 var gulp = require('gulp');
 var requireDir = require('require-dir');
 var pipelines = requireDir('./pipelines');
+var runSequence = require('run-sequence').use(gulp);
 
-gulp.task("main", ['applyJS', 'applyCSS', 'copy:assets']);
-gulp.task('default', ['clean:dest','main']);
+gulp.task("main", function(){
+  runSequence('clean:dest', 'applyJS', 'applyCSS', 'copy:assets');
+});
+gulp.task('default', ['main']);
--- pipelines/clear.js
+++ pipelines/clear.js
@@ -1,18 +1,14 @@
 "use strict";
 var gulp = require('gulp');
-var del = require('del');
-var pathType = require('path-type');
+var fs = require('fs');
+var rmdir = require('rmdir');
 
 gulp.task('clean:dest', function () {
-
-  try{
-    var checkdest = pathType.dirSync('./dest');
-
-    if(checkdest){
-      del(['./dest']);
-      console.log("Directory deleted");
+  fs.exists('./dest', function(res){
+    if(res){
+      rmdir('./dest', function(err, dir, files){
+        console.log("Dest folder has been deleted");
+      });
     }
-  }catch(err){
-    console.log(err);
-  }
+  });
 });
